f10a98998f039c069a0429f2e9b6f3347b4d9587
JA <ja.programiane@gmail.com>
2013-01-22 22:53:12 +0200

    commit 2
    add VB project

---
+++ Git Test1/Git Test1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        Label1.Visible = False
+    End Sub
+End Class
